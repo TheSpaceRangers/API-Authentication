e6592c1766827acb2a623063c1fcbf54a090959a
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-17 21:57:43 +0200

    Service Authentication Test

---
+++ src/test/java/fr/bio/apiauthentication/services/AuthenticationServiceTest.java
@@ -0,0 +1,72 @@
+package fr.bio.apiauthentication.services;
+
+import fr.bio.apiauthentication.dto.AuthenticationResponse;
+import fr.bio.apiauthentication.dto.CreateUserRequest;
+import fr.bio.apiauthentication.entities.User;
+import fr.bio.apiauthentication.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class AuthenticationServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private AuthenticationService authenticationService;
+
+    private CreateUserRequest request;
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        request = new CreateUserRequest(
+                "Charles",
+                "TRONEL",
+                "c.tronel@test.com",
+                "1234567890"
+        );
+
+        user = User.builder()
+                .firstName("Charles")
+                .lastName("Tronel")
+                .email("c.tronel@test.com")
+                .password("1234567890")
+                .build();
+    }
+
+    @Test
+    @Transactional
+    public void testRegisterUser() {
+        when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        ResponseEntity<AuthenticationResponse> response = authenticationService.register(request);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getMessage()).isEqualTo("L'utilisateur c.tronel@test.com a bien été créé !");
+
+        verify(passwordEncoder, times(1)).encode("1234567890");
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}
