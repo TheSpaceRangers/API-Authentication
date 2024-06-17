38fc1d76dffed39291f0ddb447101f683cae4b20
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-17 23:41:11 +0200

    Entity Role

---
+++ src/main/java/fr/bio/apiauthentication/entities/Role.java
@@ -0,0 +1,25 @@
+package fr.bio.apiauthentication.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role")
+    private long idRole;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    private boolean enabled;
+}
