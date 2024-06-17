1b2c3f40826c04e1da0868d6249385a093d5138a
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-17 21:37:41 +0200

    Repository User Test

---
+++ src/test/java/fr/bio/apiauthentication/repositories/UserRepositoryTest.java
@@ -0,0 +1,95 @@
+package fr.bio.apiauthentication.repositories;
+
+import fr.bio.apiauthentication.entities.User;
+
+import jakarta.transaction.Transactional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("Test User JPA Repository")
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .email("c.tronel@test.com")
+                .password("password")
+                .firstName("firstName")
+                .lastName("lastName")
+                .enabled(true)
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+        user = null;
+    }
+
+    @Test
+    @Transactional
+    public void testSaveUser() {
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
+    }
+
+    @Test
+    @Transactional
+    public void testFindById() {
+        User savedUser = userRepository.save(user);
+
+        Optional<User> foundUser = userRepository.findById(savedUser.getIdUser());
+
+        assertThat(foundUser.isPresent()).isTrue();
+        assertThat(foundUser.get().getIdUser()).isEqualTo(user.getIdUser());
+    }
+
+    @Test
+    @Transactional
+    public void testFindByEmail() {
+        User savedUser = userRepository.save(user);
+
+        Optional<User> foundUser = userRepository.findByEmail(savedUser.getEmail());
+
+        assertThat(foundUser.isPresent()).isTrue();
+    }
+
+    @Test
+    @Transactional
+    public void testFindByFirstNameAndLastName() {
+        User savedUser = userRepository.save(user);
+
+        Optional<User> foundUser = userRepository.findByFirstNameAndLastName(savedUser.getFirstName(), savedUser.getLastName());
+
+        assertThat(foundUser.isPresent()).isTrue();
+    }
+
+    @Test
+    @Transactional
+    public void testDeleteById() {
+        User savedUser = userRepository.save(user);
+
+        userRepository.deleteById(savedUser.getIdUser());
+
+        Optional<User> deletedUser = userRepository.findById(savedUser.getIdUser());
+
+        assertThat(deletedUser.isPresent()).isFalse();
+    }
+}
