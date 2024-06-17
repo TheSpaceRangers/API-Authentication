9d5eb7a8b777dc41eeea3ce4a00091c0deffd7a3
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-17 21:23:27 +0200

    Entity User Test

--- src/test/java/fr/bio/apiauthentication/entities/UserTest.java
+++ src/test/java/fr/bio/apiauthentication/entities/UserTest.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @DisplayName("Test User JPA Entity")
 @DataJpaTest
@@ -51,28 +53,47 @@ public class UserTest {
         user.setLastName("Doe");
         user.setEmail("j.doe@test.com");
         user.setPassword("password 3");
+        user.setEnabled(false);
 
         user = entityManager.persistAndFlush(user);
     }
 
     @Test
     @Transactional
-    public void testEquals() {
-        User userEquals = User.builder()
-                .email("c.tronel@test.com")
-                .password("password")
-                .firstName("firstName")
-                .lastName("lastName")
-                .enabled(true)
-                .build();
-
-        assertEquals(user, userEquals);
+    public void testEquals_SameObject() {
+        assertThat(user).isEqualTo(user);
     }
 
     @Test
     @Transactional
-    public void testHashCode() {
-        User userEquals = User.builder()
+    public void testEquals_Null() {
+        assertThat(user).isNotEqualTo(null);
+    }
+
+    @Test
+    @Transactional
+    public void testEquals_DifferentClass() {
+        assertThat(user).isNotEqualTo("This is a different object");
+    }
+
+    @Test
+    @Transactional
+    public void testEquals_DifferentFields() {
+        User differentFields = User.builder()
+                .email("c.tronel@test.com")
+                .password("password")
+                .firstName("Charles")
+                .lastName("TRONEL")
+                .enabled(true)
+                .build();
+
+        assertThat(user).isNotEqualTo(differentFields);
+    }
+
+    @Test
+    @Transactional
+    public void testEquals_SameFields() {
+        User sameFields = User.builder()
                 .email("c.tronel@test.com")
                 .password("password")
                 .firstName("firstName")
@@ -80,6 +101,52 @@ public class UserTest {
                 .enabled(true)
                 .build();
 
-        assertEquals(user.hashCode(), userEquals.hashCode());
+        assertThat(user).isEqualTo(sameFields);
+    }
+
+    @Test
+    @Transactional
+    public void testHashCode_SameObject() {
+        assertThat(user.hashCode()).isEqualTo(user.hashCode());
+    }
+
+    @Test
+    @Transactional
+    public void testHashCode_Null() {
+        assertThat(user.hashCode()).isNotEqualTo(null);
+    }
+
+    @Test
+    @Transactional
+    public void testHashCode_DifferentClass() {
+        assertThat(user.hashCode()).isNotEqualTo("This is a different object".hashCode());
+    }
+
+    @Test
+    @Transactional
+    public void testHashCode_DifferentFields() {
+        User differentFields = User.builder()
+                .email("c.tronel@test.com")
+                .password("password")
+                .firstName("Charles")
+                .lastName("TRONEL")
+                .enabled(true)
+                .build();
+
+        assertThat(user.hashCode()).isNotEqualTo(differentFields.hashCode());
+    }
+
+    @Test
+    @Transactional
+    public void testHashCode_SameFields() {
+        User sameFields = User.builder()
+                .email("c.tronel@test.com")
+                .password("password")
+                .firstName("firstName")
+                .lastName("lastName")
+                .enabled(true)
+                .build();
+
+        assertThat(user.hashCode()).isEqualTo(sameFields.hashCode());
     }
 }
