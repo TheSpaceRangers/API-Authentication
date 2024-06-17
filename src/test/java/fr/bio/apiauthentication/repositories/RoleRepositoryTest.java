7aa946eef169e7132c618e05a98d09a2381f59a7
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-17 23:57:58 +0200

    Repository Role Test

---
+++ src/test/java/fr/bio/apiauthentication/repositories/RoleRepositoryTest.java
@@ -0,0 +1,67 @@
+package fr.bio.apiauthentication.repositories;
+
+import fr.bio.apiauthentication.entities.Role;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("Test Role JPA Repository")
+@DataJpaTest
+public class RoleRepositoryTest {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    private Role role;
+
+    @BeforeEach
+    void setUp() {
+        role = Role.builder()
+                .roleName("USER")
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        roleRepository.deleteAll();
+        role = null;
+    }
+
+    @Test
+    @Transactional
+    public void testSaveRole() {
+        Role savedRole = roleRepository.save(role);
+
+        assertThat(savedRole).isNotNull();
+        assertThat(savedRole.getRoleName()).isEqualTo(role.getRoleName());
+    }
+
+    @Test
+    @Transactional
+    public void testFindByRoleName() {
+        Role savedRole = roleRepository.save(role);
+
+        Optional<Role> foundRole = roleRepository.findByRoleName(role.getRoleName());
+
+        assertThat(foundRole.isPresent()).isTrue();
+    }
+
+    @Test
+    @Transactional
+    public void testDeleteRole() {
+        Role savedRole = roleRepository.save(role);
+
+        roleRepository.deleteById(savedRole.getIdRole());
+
+        Optional<Role> foundRole = roleRepository.findById(role.getIdRole());
+
+        assertThat(foundRole.isPresent()).isFalse();
+    }
+}
