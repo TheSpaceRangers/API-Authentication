bf1c300617b7b0244f414c3ce8d02ad20ece6d34
Charles <tronel.charles@gmail.com>
2024-06-18 15:28:54 +0200

    Add login functionality to AuthenticationService

--- src/main/java/fr/bio/apiauthentication/services/AuthenticationService.java
+++ src/main/java/fr/bio/apiauthentication/services/AuthenticationService.java
@@ -1,27 +1,45 @@
 package fr.bio.apiauthentication.services;
 
+import fr.bio.apiauthentication.dto.AuthenticationRequest;
 import fr.bio.apiauthentication.dto.AuthenticationResponse;
 import fr.bio.apiauthentication.dto.CreateUserRequest;
 import fr.bio.apiauthentication.entities.Role;
+import fr.bio.apiauthentication.entities.Token;
 import fr.bio.apiauthentication.entities.User;
+import fr.bio.apiauthentication.exceptions.InvalidCredentialsException;
 import fr.bio.apiauthentication.exceptions.RoleNotFoundException;
 import fr.bio.apiauthentication.repositories.RoleRepository;
+import fr.bio.apiauthentication.repositories.TokenRepository;
 import fr.bio.apiauthentication.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.Optional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService implements IAuthenticationService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final TokenRepository tokenRepository;
 
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
+
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
 
     @Override
     public ResponseEntity<AuthenticationResponse> register(CreateUserRequest request) {
@@ -44,5 +62,71 @@ public class AuthenticationService implements IAuthenticationService {
                         .message("L'utilisateur " + user.getEmail() + " a bien été créé !")
                         .build()
         );
+    }
+
+    @Override
+    public ResponseEntity<AuthenticationResponse> login(AuthenticationRequest request) {
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.email(),
+                            request.password()
+                    )
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (AuthenticationException e) {
+            throw new InvalidCredentialsException("Email et/ou mot de passe incorecte");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+
+        final String token = jwtService.generateToken(userDetails);
+        final String refreshToken = jwtService.generateRefreshToken(userDetails);
+
+        revokeAllUserTokens(userDetails);
+        saveUserToken(userDetails, token);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Expose-Headers", "Authorization");
+        headers.add("Authorization", "Bearer " + token);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(AuthenticationResponse.builder()
+                        .message("L'utilisateur " + request.email() + " est connecté !")
+                        .build()
+                );
+    }
+
+    private void saveUserToken(
+            UserDetails userDetails,
+            String strToken
+    ) {
+        User user = userRepository.findByEmail(userDetails.getUsername()).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+
+        Token token = Token.builder()
+                .token(strToken)
+                .user(user)
+                .build();
+
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(UserDetails userDetails) {
+        User user = userRepository.findByEmail(userDetails.getUsername()).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+
+        List<Token> tokens = tokenRepository.findAllValidTokenByUser(user);
+        tokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(tokens);
     }
 }
--- src/main/java/fr/bio/apiauthentication/services/IAuthenticationService.java
+++ src/main/java/fr/bio/apiauthentication/services/IAuthenticationService.java
@@ -1,9 +1,12 @@
 package fr.bio.apiauthentication.services;
 
+import fr.bio.apiauthentication.dto.AuthenticationRequest;
 import fr.bio.apiauthentication.dto.AuthenticationResponse;
 import fr.bio.apiauthentication.dto.CreateUserRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface IAuthenticationService {
     ResponseEntity<AuthenticationResponse> register(CreateUserRequest request);
+
+    ResponseEntity<AuthenticationResponse> login(AuthenticationRequest request);
 }
