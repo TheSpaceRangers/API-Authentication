b42488f1fde4ba5688554f56769031666e8371ab
Charles <tronel.charles@gmail.com>
2024-06-18 15:24:48 +0200

    methode pour rechercher tous les tokens d'un utilisateur

--- src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
+++ src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
@@ -1,7 +1,9 @@
 package fr.bio.apiauthentication.repositories;
 
 import fr.bio.apiauthentication.entities.Token;
+import fr.bio.apiauthentication.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,11 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
     List<Token> findAllByUser_IdUser(Long idUser);
 
     List<Token> findAllByUser_Email(String email);
+
+    @Query(value = """
+        select t from Token t inner join User u
+        on t.user.idUser = u.idUser
+        where u.idUser = :idUser and (t.expired = false and t.revoked = false )
+    """)
+    List<Token> findAllValidTokenByUser(User user);
 }
