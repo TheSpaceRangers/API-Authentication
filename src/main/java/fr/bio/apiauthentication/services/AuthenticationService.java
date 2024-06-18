218dbecc182516b4c343f5b1a457f71354ded769
Charles <tronel.charles@gmail.com>
2024-06-18 10:27:13 +0200

    Role par defaut lors du register

--- src/main/java/fr/bio/apiauthentication/services/AuthenticationService.java
+++ src/main/java/fr/bio/apiauthentication/services/AuthenticationService.java
@@ -2,30 +2,42 @@ package fr.bio.apiauthentication.services;
 
 import fr.bio.apiauthentication.dto.AuthenticationResponse;
 import fr.bio.apiauthentication.dto.CreateUserRequest;
+import fr.bio.apiauthentication.entities.Role;
 import fr.bio.apiauthentication.entities.User;
+import fr.bio.apiauthentication.exceptions.RoleNotFoundException;
+import fr.bio.apiauthentication.repositories.RoleRepository;
 import fr.bio.apiauthentication.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService implements IAuthenticationService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public ResponseEntity<AuthenticationResponse> register(CreateUserRequest request) {
+        Role role = roleRepository.findByRoleName("USER").orElseThrow(
+                () -> new RoleNotFoundException("Role USER not found")
+        );
+
         User user = User.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .password(passwordEncoder.encode(request.password()))
+                .roles(Collections.singleton(role))
                 .build();
 
-        this.userRepository.save(user);
+        userRepository.save(user);
 
         return ResponseEntity.ok().body(
                 AuthenticationResponse.builder()
