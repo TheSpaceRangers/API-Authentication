8fd99e64e2ba5e136e2b4bd6086821f6b729871d
Charles <tronel.charles@gmail.com>
2024-06-18 09:55:07 +0200

    Repository Token

--- src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
+++ src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
@@ -3,7 +3,6 @@ package fr.bio.apiauthentication.repositories;
 import fr.bio.apiauthentication.entities.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
