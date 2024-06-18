7baf7686474b5c31ff6f8315e7be5f20ce4b91de
Charles <tronel.charles@gmail.com>
2024-06-18 10:07:39 +0200

    Repository Token findAllByUser

--- src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
+++ src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
@@ -3,10 +3,13 @@ package fr.bio.apiauthentication.repositories;
 import fr.bio.apiauthentication.entities.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByToken(String token);
 
-    //List<Token> findByUserId(Long idUser);
+    List<Token> findAllByUser_IdUser(Long idUser);
+
+    List<Token> findAllByUser_Email(String email);
 }
