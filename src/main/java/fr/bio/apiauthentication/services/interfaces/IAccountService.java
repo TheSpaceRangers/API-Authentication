38a381c55c4a1f64283a78af25994ee61925a6be
Charles <tronel.charles@gmail.com>
2024-06-19 14:57:48 +0200

    Add IAccountService interface

---
+++ src/main/java/fr/bio/apiauthentication/services/interfaces/IAccountService.java
@@ -0,0 +1,9 @@
+package fr.bio.apiauthentication.services.interfaces;
+
+import fr.bio.apiauthentication.dto.account.UserProfilRequest;
+import fr.bio.apiauthentication.dto.account.UserProfilResponse;
+import org.springframework.http.ResponseEntity;
+
+public interface IAccountService {
+    ResponseEntity<UserProfilResponse> getUserProfil (UserProfilRequest request);
+}
