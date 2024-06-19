ed785c2f7e56eb0c953a2718ad9fd21cff978407
Charles <tronel.charles@gmail.com>
2024-06-19 14:57:05 +0200

    Add Lombok annotations to UserProfilResponse

--- src/main/java/fr/bio/apiauthentication/dto/account/UserProfilResponse.java
+++ src/main/java/fr/bio/apiauthentication/dto/account/UserProfilResponse.java
@@ -1,9 +1,17 @@
 package fr.bio.apiauthentication.dto.account;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserProfilResponse {
     @JsonProperty("first_name")
     private String firstName;
