f18049de1a8c22a95edd57d55bba6007704b6a99
Charles <tronel.charles@gmail.com>
2024-06-19 16:56:46 +0200

    Add UpdateUserProfile functionality
    A new dto class called UpdateUserProfilRequest has been added to accommodate user profile updates. A new method updateUserProfile has been implemented in AccountService with relevant changes in the IAccountService interface. This new functionality allows for updating first name, last name, and email of a user.

--- src/main/java/fr/bio/apiauthentication/services/AccountService.java
+++ src/main/java/fr/bio/apiauthentication/services/AccountService.java
@@ -1,5 +1,6 @@
 package fr.bio.apiauthentication.services;
 
+import fr.bio.apiauthentication.dto.account.UpdateUserProfilRequest;
 import fr.bio.apiauthentication.dto.account.UserProfilRequest;
 import fr.bio.apiauthentication.dto.account.UserProfilResponse;
 import fr.bio.apiauthentication.entities.Role;
@@ -7,7 +8,7 @@ import fr.bio.apiauthentication.entities.User;
 import fr.bio.apiauthentication.repositories.UserRepository;
 import fr.bio.apiauthentication.services.interfaces.IAccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -31,6 +32,7 @@ public class AccountService implements IAccountService {
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
 
         return ResponseEntity.ok()
+                .headers(getHeaders(request.token()))
                 .body(UserProfilResponse.builder()
                         .firstName(user.getFirstName())
                         .lastName(user.getLastName())
@@ -42,5 +44,49 @@ public class AccountService implements IAccountService {
                         )
                         .build()
                 );
+    }
+
+    @Override
+    public ResponseEntity<UserProfilResponse> updateUserProfile(
+            UserProfilRequest request,
+            UpdateUserProfilRequest updateRequest
+    ) {
+        final String email = jwtService.getUsernameFromToken(request.token());
+
+        final User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+
+        if (!user.getFirstName().equals(updateRequest.firstName()))
+            user.setFirstName(updateRequest.firstName());
+
+        if (!user.getLastName().equals(updateRequest.lastName()))
+            user.setLastName(updateRequest.lastName());
+
+        if (!user.getEmail().equals(updateRequest.email()))
+            user.setEmail(updateRequest.email());
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok()
+                .headers(getHeaders(request.token()))
+                .body(UserProfilResponse.builder()
+                        .firstName(user.getFirstName())
+                        .lastName(user.getLastName())
+                        .email(user.getEmail())
+                        .roles(user.getRoles()
+                                .stream()
+                                .map(Role::getRoleName)
+                                .collect(Collectors.toList())
+                        )
+                        .build()
+                );
+    }
+
+    private HttpHeaders getHeaders(String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Expose-Headers", "Authorization");
+        headers.add("Authorization", "Bearer " + token);
+
+        return headers;
     }
 }
--- src/main/java/fr/bio/apiauthentication/services/interfaces/IAccountService.java
+++ src/main/java/fr/bio/apiauthentication/services/interfaces/IAccountService.java
@@ -1,9 +1,12 @@
 package fr.bio.apiauthentication.services.interfaces;
 
+import fr.bio.apiauthentication.dto.account.UpdateUserProfilRequest;
 import fr.bio.apiauthentication.dto.account.UserProfilRequest;
 import fr.bio.apiauthentication.dto.account.UserProfilResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface IAccountService {
     ResponseEntity<UserProfilResponse> getUserProfile(UserProfilRequest request);
+
+    ResponseEntity<UserProfilResponse> updateUserProfile(UserProfilRequest request, UpdateUserProfilRequest updateRequest);
 }
