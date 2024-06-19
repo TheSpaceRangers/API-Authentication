121cf870d18ec8d644134d432efbfaf7072d4a6f
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-19 22:16:39 +0200

    Refactor profile update method in account service
    The profile update method in account service has been simplified by merging UserProfilRequest and UpdateUserProfilRequest into a single UpdateUserProfilRequest. This significantly cleans up the controller and service methods for updating the user profile, reducing code duplication and potential points of failure.

--- src/main/java/fr/bio/apiauthentication/dto/account/UpdateUserProfilRequest.java
+++ src/main/java/fr/bio/apiauthentication/dto/account/UpdateUserProfilRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateUserProfilRequest(
+        @NotNull @NotNull String token,
         String firstName,
         String lastName,
         @Email String email
