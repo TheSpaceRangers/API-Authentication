4a273a014240de6f903a891b2e294f6dadf5f1e9
Charles <tronel.charles@gmail.com>
2024-06-19 16:58:44 +0200

    Add profile update method in AccountController
    A new method for updating user profiles has been added in the AccountController. This updateProfile method uses HTTP PUT mapping and takes both UserProfilRequest and UpdateUserProfilRequest as validated request bodies. The method relies on the accountService to process the requests and generate the response.

--- src/main/java/fr/bio/apiauthentication/controllers/AccountController.java
+++ src/main/java/fr/bio/apiauthentication/controllers/AccountController.java
@@ -1,5 +1,6 @@
 package fr.bio.apiauthentication.controllers;
 
+import fr.bio.apiauthentication.dto.account.UpdateUserProfilRequest;
 import fr.bio.apiauthentication.dto.account.UserProfilRequest;
 import fr.bio.apiauthentication.dto.account.UserProfilResponse;
 import fr.bio.apiauthentication.services.AccountService;
@@ -19,5 +20,13 @@ public class AccountController {
             @Validated @RequestBody UserProfilRequest request
     ) {
         return accountService.getUserProfile(request);
+    }
+
+    @PutMapping(value = "/profil")
+    public ResponseEntity<UserProfilResponse> updateProfile(
+            @Validated @RequestBody UserProfilRequest request,
+            @Validated @RequestBody UpdateUserProfilRequest updateRequest
+    ) {
+        return accountService.updateUserProfile(request, updateRequest);
     }
 }
