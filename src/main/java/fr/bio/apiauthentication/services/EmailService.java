c2aa1c87e7cb6971f91a9fb9a0c93822852affc3
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-20 22:44:12 +0200

    Remove unnecessary import in EmailService
    The import statement for "lombok.RequiredArgsConstructor" in the EmailService.java file was removed as it was unused. This change contributes to reducing clutter and improving the readability and maintainability of the code.

--- src/main/java/fr/bio/apiauthentication/services/EmailService.java
+++ src/main/java/fr/bio/apiauthentication/services/EmailService.java
@@ -1,7 +1,6 @@
 package fr.bio.apiauthentication.services;
 
 import fr.bio.apiauthentication.services.interfaces.IEmailService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
