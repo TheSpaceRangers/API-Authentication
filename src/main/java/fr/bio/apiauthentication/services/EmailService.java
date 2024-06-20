7cdfca30210ba5e876b84ce31fe5bc6430c8a0c2
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-20 07:53:52 +0200

    Add email service functionality
    Added the 'spring-boot-starter-mail' dependency in the pom.xml file and configured the email properties in the application.properties file. Also, created 'IEmailService' interface and implemented it in the 'EmailService' class to send emails.

---
+++ src/main/java/fr/bio/apiauthentication/services/EmailService.java
@@ -0,0 +1,25 @@
+package fr.bio.apiauthentication.services;
+
+import fr.bio.apiauthentication.services.interfaces.IEmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService implements IEmailService {
+
+    private final JavaMailSender javaMailSender = new JavaMailSenderImpl();
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        javaMailSender.send(message);
+    }
+}
