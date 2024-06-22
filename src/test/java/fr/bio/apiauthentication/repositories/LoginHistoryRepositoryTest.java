07f928ebf0e27aefbb558dc21b4d489d178c6a56
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-22 20:27:20 +0200

    Add login history tracking and testing
    This commit introduces a new feature for tracking user login history. A new entity, LoginHistory, has been created to store this information. Corresponding repository and services have also been implemented. In addition, unit tests have been added to validate this new feature, including LoginHistory tests and tests for successful authentication. Security configuration has also been updated to include a new AuthenticationSuccessFilter.

---
+++ src/test/java/fr/bio/apiauthentication/repositories/LoginHistoryRepositoryTest.java
@@ -0,0 +1,84 @@
+package fr.bio.apiauthentication.repositories;
+
+import fr.bio.apiauthentication.entities.LoginHistory;
+import fr.bio.apiauthentication.entities.Role;
+import fr.bio.apiauthentication.entities.User;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("Test LoginHistory JPA Repository")
+@DataJpaTest
+@Transactional
+public class LoginHistoryRepositoryTest {
+    @Autowired
+    private LoginHistoryRepository loginHistoryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        loginHistoryRepository.deleteAll();
+    }
+
+    @AfterEach
+    void tearDown() {
+        loginHistoryRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("Test save login history")
+    public void testSaveRole() {
+        LoginHistory loginHistory = LoginHistory.builder()
+                .user(null)
+                .ipAddress("This is IP Address")
+                .dateLogin(LocalDateTime.now())
+                .build();
+        LoginHistory savedLoginHistory = loginHistoryRepository.save(loginHistory);
+
+        assertThat(savedLoginHistory).isNotNull();
+        assertThat(savedLoginHistory.getIpAddress()).isEqualTo(loginHistory.getIpAddress());
+    }
+
+    @Test
+    @DisplayName("Test find all login history by user")
+    public void testFindByRoleName() {
+        User user = User.builder()
+                .email("c.tronel@test.properties.com")
+                .password("password")
+                .firstName("firstName")
+                .lastName("lastName")
+                .createdAt(LocalDate.now())
+                .createdBy("System")
+                .modifiedAt(LocalDate.now())
+                .modifiedBy("System")
+                .enabled(true)
+                .build();
+        userRepository.save(user);
+
+        LoginHistory loginHistory = LoginHistory.builder()
+                .user(user)
+                .ipAddress("This is IP Address")
+                .dateLogin(LocalDateTime.now())
+                .build();
+        LoginHistory savedLoginHistory = loginHistoryRepository.save(loginHistory);
+
+        List<LoginHistory> actualLogionHistory = List.of(savedLoginHistory);
+        List<LoginHistory> foundLoginHistoryList = loginHistoryRepository.findAllByUser(user);
+
+        assertThat(foundLoginHistoryList.size()).isEqualTo(1);
+        assertThat(foundLoginHistoryList).isEqualTo(actualLogionHistory);
+    }
+}
