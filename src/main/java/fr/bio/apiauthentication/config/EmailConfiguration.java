9c19d16468db816b7a576e88e185f4ca643652d4
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-22 18:46:02 +0200

    Add email configuration and update email service
    An EmailConfiguration class was added to handle specific email settings like host, port, username, and password. The EmailService class was updated to utilize this new configuration, removing the hard-coded JavaMailSender instance and replacing it with one provided by Spring's context. A new unit test, EmailServiceTest, was also added to verify the functionality of the email service.

---
+++ src/main/java/fr/bio/apiauthentication/config/EmailConfiguration.java
@@ -0,0 +1,46 @@
+package fr.bio.apiauthentication.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.properties.mail.smtp.auth}")
+    private boolean auth;
+
+    @Value("${mail.properties.mail.smtp.starttls.enable}")
+    private boolean starttls;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
+
+        return mailSender;
+    }
+
+}
