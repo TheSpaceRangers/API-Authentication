3db866ed579bcc7062662c2ea819bfe14f28a498
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-23 21:14:42 +0200

    Add UserModificationRequest DTO
    A new Data Transfer Object, UserModificationRequest, has been added to manage user modification requests. This object contains fields for email, firstName, lastName, and roles with appropriate validation constraints.

---
+++ src/main/java/fr/bio/apiauthentication/dto/admin/UserModificationRequest.java
@@ -0,0 +1,17 @@
+package fr.bio.apiauthentication.dto.admin;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record UserModificationRequest(
+        @NotNull @NotBlank @Email String email,
+        @JsonProperty("first_name") String firstName,
+        @JsonProperty("last_name") String lastName,
+        @JsonProperty("roles")List<String> roles
+) {
+
+}
