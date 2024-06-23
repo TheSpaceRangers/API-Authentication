04e4e49f3fbaf5e6369a56f295b657492757be59
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-23 02:04:36 +0200

    Add AuthenticationSuccessListener tests
    Created tests for the AuthenticationSuccessListener class to ensure its correct functionality. These tests include setting up mocks, testing the application event, and verifying the saved login history.

---
+++ src/test/java/fr/bio/apiauthentication/components/AuthenticationSuccessListenerTest.java
@@ -0,0 +1,68 @@
+package fr.bio.apiauthentication.components;
+
+import fr.bio.apiauthentication.entities.LoginHistory;
+import fr.bio.apiauthentication.entities.User;
+import fr.bio.apiauthentication.repositories.LoginHistoryRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@DisplayName("Test authentication success listener")
+public class AuthenticationSuccessListenerTest {
+    @InjectMocks
+    private AuthenticationSuccessListener listener;
+
+    @Mock
+    private LoginHistoryRepository loginHistoryRepository;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testOnApplicationEvent() {
+        User user = User.builder()
+                .email("c.tronel@test.properties.com")
+                .password("password")
+                .firstName("firstName")
+                .lastName("lastName")
+                .enabled(true)
+                .roles(null)
+                .build();
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null);
+        WebAuthenticationDetails webDetails = new WebAuthenticationDetails(request);
+        authentication.setDetails(webDetails);
+
+        AuthenticationSuccessEvent event = new AuthenticationSuccessEvent(authentication);
+
+        listener.onApplicationEvent(event);
+
+        ArgumentCaptor<LoginHistory> captor = ArgumentCaptor.forClass(LoginHistory.class);
+        verify(loginHistoryRepository).save(captor.capture());
+        LoginHistory savedLoginHistory = captor.getValue();
+
+        assertThat(savedLoginHistory).isNotNull();
+        assertThat(user).isEqualTo(savedLoginHistory.getUser());
+        assertThat(savedLoginHistory.getDateLogin()).isNotNull();
+    }
+}
