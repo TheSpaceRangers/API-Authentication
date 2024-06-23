ab87cc835d42a0aa1d5efab2ae4bf47ef4ed2fe8
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-23 19:05:50 +0200

    Add UserStructureResponse DTO
    Implemented UserStructureResponse data transfer object in the admin package. This DTO features fields related to user information including role specifics, metadata such as creation and modification data, and enabling status. It further includes a method for mapping from a User entity to this DTO structure.

---
+++ src/main/java/fr/bio/apiauthentication/dto/admin/UserStructureResponse.java
@@ -0,0 +1,69 @@
+package fr.bio.apiauthentication.dto.admin;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import fr.bio.apiauthentication.entities.Role;
+import fr.bio.apiauthentication.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserStructureResponse {
+    @JsonProperty("id_user")
+    private long idUser;
+
+    @JsonProperty("email")
+    private String email;
+
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    @JsonProperty("created_at")
+    private LocalDate createdAt;
+
+    @JsonProperty("created_by")
+    private String createdBy;
+
+    @JsonProperty("modified_at")
+    private LocalDate modifiedAt;
+
+    @JsonProperty("modified_by")
+    private String modifiedBy;
+
+    @JsonProperty("enabled")
+    private boolean enabled = true;
+
+    @JsonProperty("roles")
+    private Collection<String> roles;
+
+    public static UserStructureResponse fromRole(User user) {
+        return UserStructureResponse.builder()
+                .idUser(user.getIdUser())
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .createdAt(user.getCreatedAt())
+                .createdBy(user.getCreatedBy())
+                .modifiedAt(user.getModifiedAt())
+                .modifiedBy(user.getModifiedBy())
+                .enabled(user.isEnabled())
+                .roles(user.getRoles() != null
+                    ? user.getRoles().stream()
+                        .map(role -> role.getAuthority() + " : " + role.getDisplayName())
+                        .toList()
+                    : List.of()
+                ).build();
+    }
+}
