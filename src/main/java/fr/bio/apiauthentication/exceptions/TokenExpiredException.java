df47850f9236ae4af231a5b7e04c2bac42083979
TheSpaceRangers <tronel.charles@gmail.com>
2024-06-30 00:53:56 +0200

    Add new exception classes for token handling

---
+++ src/main/java/fr/bio/apiauthentication/exceptions/TokenExpiredException.java
@@ -0,0 +1,9 @@
+package fr.bio.apiauthentication.exceptions;
+
+public class TokenExpiredException extends RuntimeException {
+    public TokenExpiredException(
+            String message
+    ) {
+        super(message);
+    }
+}
