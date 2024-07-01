790ad975e0a47f495ab01181a8ca5df3e947e4d7
TheSpaceRangers <tronel.charles@gmail.com>
2024-07-01 23:10:27 +0200

    Remove unused PasswordEncoder import
    The import for PasswordEncoder from springframework.security.crypto.password was removed in AccountService.java file. This import is no longer used, which improves the readability and organization of the code.

--- src/main/java/fr/bio/apiauthentication/services/AccountService.java
+++ src/main/java/fr/bio/apiauthentication/services/AccountService.java
@@ -11,7 +11,6 @@ import fr.bio.apiauthentication.services.interfaces.IAccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
