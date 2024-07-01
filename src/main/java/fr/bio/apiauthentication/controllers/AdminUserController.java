74bcf02438ab62cfbc18f1bb13ce2a940dcaaad8
TheSpaceRangers <tronel.charles@gmail.com>
2024-07-01 21:55:45 +0200

    Refine error message in AdminUserController
    This commit refines the handling of an invalid "status" parameter within the AdminUserController. The IllegalArgumentException message now properly formats an error message to express the invalidity of the parameter.

--- src/main/java/fr/bio/apiauthentication/controllers/AdminUserController.java
+++ src/main/java/fr/bio/apiauthentication/controllers/AdminUserController.java
@@ -54,7 +54,7 @@ public class AdminUserController {
         else if (action.equalsIgnoreCase("deactivate"))
             status = false;
         else
-            throw new IllegalArgumentException(Messages.STATUS_PARAMETER_INVALID.formatMessage(""));
+            throw new IllegalArgumentException(Messages.STATUS_PARAMETER_INVALID.formatMessage(Messages.STATUS_PARAMETER_INVALID.formatMessage()));
 
         return adminUserService.updateUserStatus(token, request, status);
     }
