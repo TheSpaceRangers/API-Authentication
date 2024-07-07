ea8bcfd1cdf1e7ab920fa0d6358e797cd48e5f59
TheSpaceRangers <tronel.charles@gmail.com>
2024-07-07 19:04:18 +0200

    Update TokenRepository test with additional operations
    TokenRepositoryTest.java has been enhanced with additional operations for token testing. Random values have been applied for setting up the tests and additional assertions have been added for validation. Furthermore, a new function for generating tokens has been created. TokenRepository.java interface has been updated with a method for fetching expired and revoked tokens.

--- src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
+++ src/main/java/fr/bio/apiauthentication/repositories/TokenRepository.java
@@ -4,8 +4,6 @@ import fr.bio.apiauthentication.entities.Token;
 import fr.bio.apiauthentication.entities.User;
 import fr.bio.apiauthentication.enums.TokenType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,11 +17,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 
     List<Token> findByUserAndTypeAndExpiredFalseAndRevokedFalse(User user, TokenType type);
 
-    void deleteByUserAndType(User user, TokenType type);
+    List<Token> findByUserAndTypeAndExpiredTrueAndRevokedTrue(User user, TokenType type);
 
-    @Query(value = """
-        select t from Token t inner join t.user u
-        where u.idUser = :idUser and (t.expired = false and t.revoked = false)
-    """)
-    List<Token> findAllValidTokenByUser(@Param("idUser") Long idUser);
+    void deleteByUserAndType(User user, TokenType type);
 }
--- src/test/java/fr/bio/apiauthentication/repositories/TokenRepositoryTest.java
+++ src/test/java/fr/bio/apiauthentication/repositories/TokenRepositoryTest.java
@@ -2,7 +2,9 @@ package fr.bio.apiauthentication.repositories;
 
 import fr.bio.apiauthentication.entities.Token;
 import fr.bio.apiauthentication.entities.User;
+import fr.bio.apiauthentication.enums.TokenType;
 import jakarta.transaction.Transactional;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,18 +28,37 @@ public class TokenRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    private Token token;
     private User user;
+
+    private String tokenString;
+    private TokenType tokenType;
+    private boolean expired;
+    private boolean revoked;
 
     @BeforeEach
     void setUp() {
+        tokenString = RandomStringUtils.randomAlphanumeric(100);
+        tokenType = Arrays.stream(TokenType.values()).toList().get((int) (Math.random() * TokenType.values().length));
+        expired = Boolean.parseBoolean(RandomStringUtils.randomNumeric(0, 1));
+        revoked = Boolean.parseBoolean(RandomStringUtils.randomNumeric(0, 1));
+
         user = User.builder()
-                .email("c.tronel@test.properties.com")
-                .password("password")
-                .firstName("firstName")
-                .lastName("lastName")
+                .email(RandomStringUtils.randomAlphanumeric(10) + "@test.com")
+                .password(RandomStringUtils.randomAlphanumeric(30))
+                .firstName(RandomStringUtils.randomAlphanumeric(20))
+                .lastName(RandomStringUtils.randomAlphanumeric(20))
                 .enabled(true)
                 .build();
         userRepository.save(user);
+
+        token = Token.builder()
+                .token(tokenString)
+                .type(tokenType)
+                .user(user)
+                .expired(expired)
+                .revoked(revoked)
+                .build();
     }
 
     @AfterEach
@@ -45,95 +67,92 @@ public class TokenRepositoryTest {
         tokenRepository.deleteAll();
 
         user = null;
+        token = null;
     }
 
     @Test
     @DisplayName("Test save token")
     public void testSaveToken() {
-        Token token = Token.builder()
-                .token("Save token")
-                .user(user)
-                .build();
         Token savedToken = tokenRepository.save(token);
 
         assertThat(savedToken).isNotNull();
+        assertThat(savedToken.getToken()).isEqualTo(tokenString);
+        assertThat(savedToken.getType()).isEqualTo(tokenType);
+        assertThat(savedToken.getUser()).isEqualTo(user);
+        assertThat(savedToken.isExpired()).isEqualTo(expired);
+        assertThat(savedToken.isRevoked()).isEqualTo(revoked);
     }
 
     @Test
     @DisplayName("Test find token by token")
     public void testFindByToken() {
-        Token token = Token.builder()
-                .token("Find token")
-                .user(user)
-                .build();
         Token savedToken = tokenRepository.save(token);
 
         Optional<Token> foundToken = tokenRepository.findByToken(savedToken.getToken());
 
         assertThat(foundToken.isPresent()).isTrue();
+
+        if (foundToken.isPresent()) {
+            assertThat(foundToken.get().getToken()).isEqualTo(tokenString);
+            assertThat(foundToken.get().getType()).isEqualTo(tokenType);
+            assertThat(foundToken.get().getUser()).isEqualTo(user);
+            assertThat(foundToken.get().isExpired()).isEqualTo(expired);
+            assertThat(foundToken.get().isRevoked()).isEqualTo(revoked);
+        }
     }
 
     @Test
     @DisplayName("Test find token by user id")
     public void testFindByUser_IdUser() {
-        Token token = Token.builder()
-                .token("Find token by id")
-                .user(user)
-                .build();
-        Token savedToken = tokenRepository.save(token);
+        Token savedToken_1 = tokenRepository.save(token);
+        Token savedToken_2 = tokenRepository.save(generateToken(false, false));
 
-        List<Token> actualTokens = List.of(savedToken);
+        List<Token> exceptedTokens = List.of(savedToken_1, savedToken_2);
         List<Token> foundTokens = tokenRepository.findAllByUser_IdUser(user.getIdUser());
 
-        assertThat(actualTokens).isEqualTo(foundTokens);
+        assertThat(foundTokens).isEqualTo(exceptedTokens);
+        assertThat(foundTokens.size()).isEqualTo(exceptedTokens.size());
+        assertThat(foundTokens.get(0)).isEqualTo(savedToken_1);
+        assertThat(foundTokens.get(1)).isEqualTo(savedToken_2);
     }
 
     @Test
     @DisplayName("Test find token by user email")
     public void testFindByUser_Email() {
-        Token token = Token.builder()
-                .token("Find token by email")
-                .user(user)
-                .build();
-        Token savedToken = tokenRepository.save(token);
+        Token savedToken_1 = tokenRepository.save(token);
+        Token savedToken_2 = tokenRepository.save(generateToken(false, false));
 
-        List<Token> actualTokens = List.of(savedToken);
+        List<Token> exceptedTokens = List.of(savedToken_1, savedToken_2);
         List<Token> foundTokens = tokenRepository.findAllByUser_Email(user.getEmail());
 
-        assertThat(actualTokens).isEqualTo(foundTokens);
+        assertThat(foundTokens).isEqualTo(exceptedTokens);
+        assertThat(foundTokens.size()).isEqualTo(exceptedTokens.size());
+        assertThat(foundTokens.get(0)).isEqualTo(savedToken_1);
+        assertThat(foundTokens.get(1)).isEqualTo(savedToken_2);
     }
 
     @Test
     @DisplayName("Test find all valid token by user")
-    public void testFindAllValidTokenByUser() {
-        Token expiredToken = Token.builder()
-                .token("Find all valid token expired")
-                .expired(true)
-                .revoked(true)
-                .user(user)
-                .build();
-        tokenRepository.save(expiredToken);
-        Token validToken = Token.builder()
-                .token("Find all valid token")
-                .user(user)
-                .build();
-        tokenRepository.save(validToken);
+    public void testFindByUserAndTypeAndExpiredAndRevoked() {
+        Token expiredToken = tokenRepository.save(generateToken(true, true));
+        Token validToken = tokenRepository.save(generateToken(false, false));
 
+        List<Token> exceptedTokensValid = List.of(validToken);
+        List<Token> exceptedTokensExpired = List.of(expiredToken);
 
-        List<Token> actualTokens = List.of(validToken);
-        List<Token> foundTokens = tokenRepository.findAllValidTokenByUser(user.getIdUser());
+        List<Token> foundedTokensValid = tokenRepository.findByUserAndTypeAndExpiredFalseAndRevokedFalse(user, TokenType.BEARER);
+        List<Token> foundedTokensExpired = tokenRepository.findByUserAndTypeAndExpiredTrueAndRevokedTrue(user, TokenType.BEARER);
 
-        assertThat(actualTokens).isEqualTo(foundTokens);
+        assertThat(foundedTokensValid).isEqualTo(exceptedTokensValid);
+        assertThat(foundedTokensValid).usingRecursiveComparison().isEqualTo(exceptedTokensValid);
+
+        assertThat(foundedTokensExpired).isEqualTo(exceptedTokensExpired);
+        assertThat(foundedTokensExpired).usingRecursiveComparison().isEqualTo(exceptedTokensExpired);
     }
-
 
     @Test
     @DisplayName("Test delete token")
     public void testDeleteToken() {
-        Token token = Token.builder()
-                .token("Delete token")
-                .user(user)
-                .build();
         Token savedToken = tokenRepository.save(token);
 
         tokenRepository.delete(savedToken);
@@ -141,5 +160,30 @@ public class TokenRepositoryTest {
         Optional<Token> foundToken = tokenRepository.findByToken(savedToken.getToken());
 
         assertThat(foundToken.isPresent()).isFalse();
+    }
+
+    @Test
+    @DisplayName("Test delete token")
+    public void testDeleteByUserAndType() {
+        Token savedToken = tokenRepository.save(token);
+
+        tokenRepository.deleteByUserAndType(user, tokenType);
+
+        Optional<Token> foundToken = tokenRepository.findByToken(savedToken.getToken());
+
+        assertThat(foundToken.isPresent()).isFalse();
+    }
+
+    private Token generateToken(
+            boolean expired,
+            boolean revoked
+    ) {
+        return Token.builder()
+                .token(RandomStringUtils.randomAlphanumeric(100))
+                .type(TokenType.BEARER)
+                .user(user)
+                .revoked(expired)
+                .expired(revoked)
+                .build();
     }
 }
