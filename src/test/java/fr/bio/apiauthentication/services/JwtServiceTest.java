6f368a298efb4e550b8a33a7c0b113a6f0701164
TheSpaceRangers <tronel.charles@gmail.com>
2024-07-07 22:42:29 +0200

    Update JwtServiceTest with additional testing methods
    Enhancements made to JwtServiceTest include testing methods for generating token, token claims, username, and token validation. Updated the test setup with user and role data model. Utilized randomized strings and values for more dynamic testing scenarios and better consistency.

--- src/test/java/fr/bio/apiauthentication/services/JwtServiceTest.java
+++ src/test/java/fr/bio/apiauthentication/services/JwtServiceTest.java
@@ -1,8 +1,13 @@
 package fr.bio.apiauthentication.services;
 
+import fr.bio.apiauthentication.entities.Role;
+import fr.bio.apiauthentication.entities.User;
+import fr.bio.apiauthentication.repositories.RoleRepository;
+import fr.bio.apiauthentication.repositories.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,27 +17,73 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 @DisplayName("Test JWT Service")
 public class JwtServiceTest {
+    private static final LocalDate NOW = LocalDate.now();
+
+    private static final Date NOW_DATE = new Date(System.currentTimeMillis());
+
+    private static final Random RANDOM = new Random();
+
     @InjectMocks
     private JwtService jwtService;
 
     @Mock
     private UserDetails userDetails;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private DateUtils dateUtils;
+
+    private User user;
+
+    private Long expiration;
+
+    private String secretKey;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        jwtService.setSecretKey("sMcZfQWkvZ7cjmvdxERDyxElXP+LBE0NfQD4V1eyJuQrJ4DsUJLsJXXHis+spgyNV3+gkLT0o8SS3HU3rDucbFeH1BB10jzqlj1jmYiwNEgpnmV/YYsm8rbRYRB+VJ6tL/01M2UhrHqQCiGmQzSbLUKiXpDiFk6G0ihCL0cCsTme82k59n2nR/95rBKnDtcur");
+        expiration = (long) ((1 + RANDOM.nextInt(10)) * 60 * 1000);
+
+        Role role = Role.builder()
+                .authority(RandomStringUtils.randomAlphanumeric(10).toUpperCase())
+                .displayName(RandomStringUtils.randomAlphanumeric(20))
+                .description(RandomStringUtils.randomAlphanumeric(20))
+                .modifiedAt(NOW)
+                .modifiedBy(RandomStringUtils.randomAlphanumeric(20))
+                .enabled(true)
+                .build();
+        roleRepository.save(role);
+
+        user = User.builder()
+                .email(RandomStringUtils.randomAlphanumeric(5).toUpperCase())
+                .password(RandomStringUtils.randomAlphanumeric(30))
+                .firstName(RandomStringUtils.randomAlphanumeric(20))
+                .lastName(RandomStringUtils.randomAlphanumeric(20))
+                .createdAt(NOW)
+                .createdBy(RandomStringUtils.randomAlphanumeric(20))
+                .modifiedAt(NOW)
+                .modifiedBy(RandomStringUtils.randomAlphanumeric(20))
+                .enabled(true)
+                .roles(List.of(role))
+                .build();
+        userRepository.save(user);
+
+        secretKey = RandomStringUtils.randomAlphanumeric(256);
+        jwtService.setSecretKey(secretKey);
         jwtService.setJwtExpiration(1000 * 60 * 60);
         jwtService.init();
     }
@@ -40,80 +91,75 @@ public class JwtServiceTest {
     @Test
     @DisplayName("Test generate token")
     void testGenerateToken() {
-        when(userDetails.getUsername()).thenReturn("testUser");
-        when(userDetails.getAuthorities()).thenReturn(List.of());
+        when(userDetails.getUsername()).thenReturn(user.getEmail());
+        when(dateUtils.getCurrentDate()).thenReturn(NOW_DATE);
 
-        String token = jwtService.generateToken(userDetails);
-
-        assertThat(token).isNotNull();
-    }
-
-    @Test
-    @DisplayName("Test generate token")
-    void testGenerateToken_Expiration() {
-        when(userDetails.getUsername()).thenReturn("testUser");
-        when(userDetails.getAuthorities()).thenReturn(List.of());
-
-        String token = jwtService.generateToken(userDetails);
+        final String token = jwtService.generateToken(userDetails);
 
         assertThat(token).isNotNull();
+        assertThat(jwtService.getUsernameFromToken(token)).isEqualTo(user.getEmail());
+        assertThat(jwtService.getExpirationDateFromToken(token)).isAfter(NOW_DATE);
+
     }
 
     @Test
     @DisplayName("Test generate token")
     void testGenerateToken_Claims() {
-        when(userDetails.getUsername()).thenReturn("testUser");
-        when(userDetails.getAuthorities()).thenReturn(List.of());
+        final Map<String, Object> claims = new HashMap<>();
+        claims.put("Authorities", userDetails.getAuthorities());
 
-        String token = jwtService.generateToken(new HashMap<>(), userDetails);
+        when(userDetails.getUsername()).thenReturn(user.getEmail());
+        when(dateUtils.getCurrentDate()).thenReturn(NOW_DATE);
+
+        final String token = jwtService.generateToken(claims, userDetails);
 
         assertThat(token).isNotNull();
+        assertThat(jwtService.getUsernameFromToken(token)).isEqualTo(user.getEmail());
+        assertThat(jwtService.getExpirationDateFromToken(token)).isAfter(NOW_DATE);
     }
 
     @Test
     @DisplayName("Test get username")
     void testGetUsernameFromToken() {
-        String token = createTestToken(3600000);
+        final String token = generateJwtToken(expiration);
 
-        String username = jwtService.getUsernameFromToken(token);
+        when(dateUtils.getCurrentDate()).thenReturn(NOW_DATE);
 
-        assertThat(username).isEqualTo("testUser");
-    }
+        final String username = jwtService.getUsernameFromToken(token);
 
-    @Test
-    @DisplayName("Test get expiration date")
-    void testGetExpirationDateFromToken() {
-        String token = createTestToken(3600000);
-
-        Date expirationDate = jwtService.getExpirationDateFromToken(token);
-
-        assertThat(expirationDate).isNotNull();
-        assertThat(expirationDate.after(new Date())).isTrue();
+        assertThat(username).isEqualTo(user.getUsername());
+        assertThat(jwtService.getUsernameFromToken(token)).isEqualTo(user.getEmail());
+        assertThat(jwtService.getExpirationDateFromToken(token)).isAfter(NOW_DATE);
     }
 
     @Test
     @DisplayName("Test is valid token")
     void testValidateToken() {
-        when(userDetails.getUsername()).thenReturn("testUser");
-        String token = createTestToken(3600000);
+        final String token = generateJwtToken(expiration);
 
-        boolean isValid = jwtService.validateToken(token, userDetails);
+        when(userDetails.getUsername()).thenReturn(user.getEmail());
 
-        assertThat(isValid).isTrue();
+        assertThat(jwtService.validateToken(token, userDetails)).isTrue();
     }
 
-    private String createTestToken(long expiration) {
+    private String generateJwtToken(long expiration) {
         Map<String, Object> claims = new HashMap<>();
 
-        byte[] keyBytes = Decoders.BASE64.decode("sMcZfQWkvZ7cjmvdxERDyxElXP+LBE0NfQD4V1eyJuQrJ4DsUJLsJXXHis+spgyNV3+gkLT0o8SS3HU3rDucbFeH1BB10jzqlj1jmYiwNEgpnmV/YYsm8rbRYRB+VJ6tL/01M2UhrHqQCiGmQzSbLUKiXpDiFk6G0ihCL0cCsTme82k59n2nR/95rBKnDtcur");
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         SecretKey key = Keys.hmacShaKeyFor(keyBytes);
 
         return Jwts.builder()
                 .claims(claims)
-                .subject("testUser")
-                .issuedAt(new Date(System.currentTimeMillis()))
+                .subject(user.getEmail())
+                .issuedAt(NOW_DATE)
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(key, Jwts.SIG.HS256)
                 .compact();
+    }
+
+    private static class DateUtils {
+        public Date getCurrentDate() {
+            return new Date();
+        }
     }
 }
