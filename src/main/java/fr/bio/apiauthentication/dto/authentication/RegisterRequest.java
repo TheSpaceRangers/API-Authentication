97cf131c2fecfbb01951847e29b30d90a2f9817a
TheSpaceRangers <tronel.charles@gmail.com>
2024-09-18 12:36:55 +0200

    Add JSON property annotations to RegisterRequest fields
    This change ensures that the field names in JSON request bodies match the specified naming conventions. It improves compatibility with front-end components or other services expecting these particular field names.

--- src/main/java/fr/bio/apiauthentication/dto/authentication/RegisterRequest.java
+++ src/main/java/fr/bio/apiauthentication/dto/authentication/RegisterRequest.java
@@ -1,13 +1,14 @@
 package fr.bio.apiauthentication.dto.authentication;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RegisterRequest(
-        @NotNull @NotBlank String firstName,
-        @NotNull @NotBlank String lastName,
-        @NotNull @NotBlank @Email String email,
-        @NotNull @NotBlank String password
+        @NotNull @NotBlank @JsonProperty("first_name") String firstName,
+        @NotNull @NotBlank @JsonProperty("last_name") String lastName,
+        @NotNull @NotBlank @JsonProperty("email") @Email String email,
+        @NotNull @NotBlank @JsonProperty("password") String password
 ) {
 }
