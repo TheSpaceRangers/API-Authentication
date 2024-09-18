3aa4feb7149655d62e7a3648a383a8237a64e9a4
TheSpaceRangers <tronel.charles@gmail.com>
2024-09-18 11:59:25 +0200

    Add WebConfig for CORS support
    Introduce WebConfig class to handle CORS configuration specifically for the /api-auth/** endpoints. This configuration allows requests from http://localhost:4200 with various HTTP methods and all headers, ensuring credentials are supported.

---
+++ src/main/java/fr/bio/apiauthentication/config/WebConfig.java
@@ -0,0 +1,16 @@
+package fr.bio.apiauthentication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api-auth/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
